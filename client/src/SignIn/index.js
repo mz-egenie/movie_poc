53c9d7fcb2b7ec5bff41a18090f55ff2ae6a92f2
Muhammad Muzammil <muzamil.egenie@gmail.com>
2023-12-29 11:39:57 +0500

    Add login info in signin

--- client/src/SignIn/index.js
+++ client/src/SignIn/index.js
@@ -1,43 +1,47 @@
 import React from "react";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="signin_main">
-    <div className="signin">
-      <h2 className="signin_heading">Sign in</h2>
-      <div>
-        <input
-          className="signin_inputField"
-          required
-          placeholder="Email"
-          type="email"
-          name="email"
-          // value={""}
-          // onChange={(e) => {}}
-        />
+      <div className="signin">
+        <h2 className="signin_heading">Sign in</h2>
+        <div>
+          <input
+            className="signin_inputField"
+            required
+            placeholder="Email"
+            type="email"
+            name="email"
+            value={"user@example.com"}
+            // onChange={(e) => {}}
+          />
+        </div>
+        <div>
+          <input
+            className="signin_inputField"
+            required
+            placeholder="Password"
+            type="passsword"
+            name="password"
+            value={"123456789"}
+            // onChange={(e) => {}}
+          />
+        </div>
+        <div className="remember">
+          <input type="checkbox" onChange={(e) => {}} />
+          <p>Remember me</p>
+        </div>
+        <button
+          className="login_btn"
+          onClick={() => {
+            navigate("/my-movies");
+          }}
+        >
+          Login
+        </button>
       </div>
-      <div>
-        <input
-          className="signin_inputField"
-          required
-          placeholder="Password"
-          type="email"
-          name="email"
-          // value={""}
-          // onChange={(e) => {}}
-        />
-      </div>
-      <div className="remember">
-      <input
-          type="checkbox"
-          onChange={(e) => {}}
-        />
-        <p>Remember me</p>
-      </div>
-      <button className="login_btn" onClick={()=>{navigate('/my-movies')}}>Login</button>
-    </div>
     </div>
   );
 }
